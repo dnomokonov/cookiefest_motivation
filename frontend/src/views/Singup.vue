37969a282b093666ad6a0ce240189d45e32543d8
dnomokonov <zmnomokon715@gmail.com>
2024-12-01 03:08:07 +0700

    Update file

--- frontend/src/views/Singup.vue
+++ frontend/src/views/Singup.vue
@@ -1,94 +1,98 @@
 <script setup>
-  import { ref } from 'vue'
-  import axios from 'axios'
   import Header from '@components/TheHeader.vue'
   import Button from '@components/BaseButton.vue'
-  
+  import { ref } from 'vue'
+  import axios from 'axios'
+
   const fullName = ref('')
-  const username = ref('')
+  const login = ref('')
   const email = ref('')
   const branch = ref('')
-  const password = ref('');
+  const password = ref('')
   const confirmPassword = ref('')
-  const errorMessage = ref('')
-  const isLoading = ref(false)
 
-  const handleRegistration = async() => {
-      errorMessage.value = ''
+  const handleRegister = async () => {
+    if (!fullName.value || !login.value || !email.value || !password.value || !confirmPassword.value) {
+      alert('Пожалуйста, заполните все поля.')
+      return
+    }
 
-      if (password.value !== confirmPassword.value) {
-        errorMessage.value = 'Пароли не совпадают.';
-        return;
+    if (password.value !== confirmPassword.value) {
+      alert('Пароли не совпадают.')
+      return
+    }
+
+    const [name, surname, patronymic] = fullName.value.split(' ')
+
+    try {
+      const response = await axios.post(import.meta.env.VITE_SERVER_API + '/signup', {
+        name,
+        surname,
+        patronymic,
+        login: login.value,
+        email: email.value,
+        branch: branch.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value,
+      });
+
+      if (response.data.success) {
+        alert('Регистрация прошла успешно!');
       }
-
-      isLoading.value = true
-
-      try {
-          const response = await axios.post(import.meta.env.VITE_SERVER_API + '/reg', {
-            fullName: fullName.value,
-            username: username.value,
-            email: email.value,
-            branch: branch.value,
-            password: password.value,
-          })
-
-          console.log('Успешная регистрация:', response.data)
-          window.location.href = '/auth/login'
-
-      } catch(error) {
-          if (error.response && error.response.data) {
-              errorMessage.value = error.response.data.message || 'Ошибка авторизации.'
-          } else {
-              errorMessage.value = 'Произошла ошибка. Попробуйте снова позже.'
-          }
-
-          alert(errorMessage.value)
-
-          console.error(error)
-      } finally {
-          isLoading.value = false
+    } catch (error) {
+      if (error.response) {
+        const { data } = error.response;
+        if (data.emailMessage === false) {
+          alert('Этот email уже используется.');
+        } else if (data.loginMessage === false) {
+          alert('Этот логин уже занят.');
+        } else {
+          alert('Произошла ошибка: ' + data.description);
+        }
+      } else {
+        alert('Произошла ошибка: ' + error.message);
       }
+    }
   }
 </script>
 
 <template>
   <Header :isAuthenticated="false" backgroundColor="#EFECE7" />
-  <div class="singup_block">
+  <div class="register_block">
     <div class="content">
-      <div class="singup_from">
+      <div class="register_form">
         <h1>Регистрация</h1>
-        <form @submit.prevent="handleRegistration">
+        <form @submit.prevent="handleRegister">
           <input v-model="fullName" type="text" placeholder="ФИО" required>
-          <input v-model="username" type="text" placeholder="Логин" required>
-          <input v-model="email" type="email" placeholder="Почта" required>
+          <input v-model="login" type="text" placeholder="Логин" required>
+          <input v-model="email" type="email" placeholder="Электронная почта" required>
           <input v-model="branch" type="text" placeholder="Филиал" required>
           <input v-model="password" type="password" placeholder="Пароль" required>
           <input v-model="confirmPassword" type="password" placeholder="Повторите пароль" required>
           <div class="form_btn">
-            <Button backgroundColor="#6E7F91" hoverColor="#8793A3"
-            activeColor="#5A6B7A" titleButton="Зарегистрироваться" style="padding: 12px;"/>
+            <Button backgroundColor="#6E7F91" hoverColor="#8793A3" activeColor="#5A6B7A" titleButton="Зарегистрироваться" style="padding: 12px;"/>
           </div>
         </form>
         <p>Уже есть аккаунт? 
           <a href="/auth/login">Тогда войдите</a>
         </p>
       </div>
-      <div class="singup_image">
-        <img src="@assets/people2.png" alt="people2">
+      <div class="register_image">
+        <img src="@assets/people2.png" alt="register preview">
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .singup_block {
+  .register_block {
     width: 100%;
     height: 90vh;
     display: flex;
     justify-content: center;
   }
 
-  .singup_block .content {
+  .register_block .content {
     width: 100%;
     max-width: 1280px;
     display: flex;
@@ -96,23 +100,23 @@
     align-items: center;
   }
 
-  .singup_from {
+  .register_form {
     display: flex;
     flex-direction: column;
   }
 
-  .singup_from h1 {
+  .register_form h1 {
     font-size: 35px;
     margin-bottom: 20px;
   }
 
-  .singup_from form {
+  .register_form form {
     display: flex;
     flex-direction: column;
     gap: 15px;
   }
 
-  .singup_from form input {
+  .register_form form input {
     width: 350px;
     height: 30px;
     font-size: 14px;
@@ -123,23 +127,22 @@
     background-color: #E8E8E8;
   }
 
-  .singup_from form .form_btn {
+  .register_form form .form_btn {
     display: flex;
     justify-content: center;
   }
 
-  .singup_from p {
+  .register_form p {
     text-align: center;
     margin-top: 20px;
     font-size: 14px;
   }
 
-  .singup_from p a {
+  .register_form p a {
     text-decoration: underline;
   }
 
-  .singup_image img {
+  .register_image img {
     width: 680px;
   }
-
 </style>
